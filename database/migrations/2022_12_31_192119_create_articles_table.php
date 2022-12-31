3e3a483ce48144f23d63e0b5124318a3c3d477f2
Volodymyr Yurchenko <discover@.url.net>
2022-12-31 21:43:10 +0200

    db: add articles migraion

---
+++ database/migrations/2022_12_31_192119_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string('title');
+            $table->string('description');
+            $table->string('url')->unique();
+            $table->string('h1');
+            $table->string('image')->nullable();
+            $table->text('intro')->nullable();
+            $table->text('content')->nullable();
+            $table->tinyInteger('menu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
